3fd81d1f15085acc6635fbfdd1172db0f4156e79
tlarrode-gif <tlarrode@gmail.com>
2026-06-04 16:52:31 +0200

    Enable ISDIN operational compact panel view

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./calendar-visual.css";
 import "./forms-visual.css";
 import "./campaigns-visual.css";
 import "./final-ui-shell.css";
+import "./isdin-operational-view.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -33,6 +34,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </nav>
         </div>
         {children}
+        <script src="/isdin-operational-view.js" defer></script>
       </body>
     </html>
   );
