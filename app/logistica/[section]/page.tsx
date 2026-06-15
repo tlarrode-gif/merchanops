1e26bc0cc41eb0dea7406d57ac092ed44df9294b
tlarrode-gif <tlarrode@gmail.com>
2026-06-15 10:50:24 +0200

    Route logistics requests through operational view

--- app/logistica/[section]/page.tsx
+++ app/logistica/[section]/page.tsx
@@ -1,5 +1,10 @@
 import { LogisticsClient } from "../logistics-client";
+import { SolicitudesLogisticaClient } from "../solicitudes/solicitudes-logistica-client";
 
 export default function LogisticsSectionPage({ params, searchParams }: { params: { section: string }; searchParams?: { id?: string } }) {
+  if (params.section === "solicitudes") {
+    return <SolicitudesLogisticaClient detailId={searchParams?.id} />;
+  }
+
   return <LogisticsClient section={params.section} detailId={searchParams?.id} />;
 }
